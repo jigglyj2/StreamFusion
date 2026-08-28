b5833ba45b98ba64bef4a4f307575d8108ea3bb8
jigglyj2 <jordanistryhardswe@gmail.com>
2026-08-28 11:10:03 -0500

    test: verify collection cardinality parity

---
+++ streamfusion-flink-sql-tests/src/test/java/tech/streamfusion/flink/sql/CollectionFunctionParityTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2026 StreamFusion Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tech.streamfusion.flink.sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.types.Row;
+import org.junit.jupiter.api.Test;
+import tech.streamfusion.flink.StreamFusionPlannerFactory;
+import tech.streamfusion.flink.planner.StreamFusionPlanningDiagnostics;
+
+class CollectionFunctionParityTest extends SqlParityTestSupport {
+    @Test
+    void arrayCardinalityMatchesFlinkByteForByte() throws Exception {
+        assertDataStreamParity(
+                "SELECT CARDINALITY(metric) FROM array_input",
+                Types.OBJECT_ARRAY(Types.INT),
+                DataTypes.ARRAY(DataTypes.INT()),
+                Arrays.asList(
+                        Row.of((Object) new Integer[] {1, null, 3}), Row.of((Object) new Integer[] {}), Row.of((Object)
+                                null)),
+                "array_input");
+
+        assertNativeCalcRan();
+    }
+
+    @Test
+    void mapCardinalityMatchesFlinkByteForByte() throws Exception {
+        Map<String, Integer> populated = new LinkedHashMap<>();
+        populated.put("a", 1);
+        populated.put("b", null);
+
+        assertDataStreamParity(
+                "SELECT CARDINALITY(metric) FROM map_input",
+                Types.MAP(Types.STRING, Types.INT),
+                DataTypes.MAP(DataTypes.STRING().notNull(), DataTypes.INT()),
+                Arrays.asList(Row.of(populated), Row.of(new LinkedHashMap<>()), Row.of((Object) null)),
+                "map_input");
+
+        assertNativeCalcRan();
+    }
+
+    @Test
+    void nestedArrayCardinalityFallsBackWithSemanticReason() throws Exception {
+        assertDataStreamParity(
+                "SELECT CARDINALITY(metric) FROM nested_array_input",
+                Types.OBJECT_ARRAY(Types.OBJECT_ARRAY(Types.INT)),
+                DataTypes.ARRAY(DataTypes.ARRAY(DataTypes.INT())),
+                Arrays.asList(
+                        Row.of((Object) new Integer[][] {{1, 2}, {3}}),
+                        Row.of((Object) new Integer[][] {}),
+                        Row.of((Object) null)),
+                "nested_array_input");
+
+        assertThat(StreamFusionPlannerFactory.nativeCalcBatchCount()).isZero();
+        assertThat(StreamFusionPlanningDiagnostics.explain())
+                .contains("DataFusion recursively counts leaf elements while Flink counts the outer array");
+    }
+
+    private static void assertNativeCalcRan() {
+        assertThat(StreamFusionPlannerFactory.nativeCalcBatchCount())
+                .withFailMessage(StreamFusionPlanningDiagnostics.explain())
+                .isGreaterThan(0);
+    }
+}
