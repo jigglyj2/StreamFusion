1c1d08c70412fa5843438d5616430f99520478b5
jigglyj2 <jordanistryhardswe@gmail.com>
2026-08-28 10:17:03 -0500

    test: prove zero-copy calc handoff

--- streamfusion-native/src/lib.rs
+++ streamfusion-native/src/lib.rs
@@ -75,6 +75,30 @@ mod tests {
         }
     }
 
+    fn chained_identity_plan() -> proto::NativePlan {
+        let mut plan = identity_plan();
+        let inner = plan.root.take().unwrap();
+        let integer_type = proto::LogicalType {
+            nullable: false,
+            r#type: Some(proto::logical_type::Type::Integer(proto::EmptyType {})),
+        };
+        plan.root = Some(proto::Operator {
+            operator: Some(proto::operator::Operator::Calc(Box::new(proto::Calc {
+                input: Some(Box::new(inner)),
+                projections: vec![proto::Expression {
+                    expression: Some(proto::expression::Expression::InputReference(
+                        proto::InputReference {
+                            index: 0,
+                            r#type: Some(integer_type),
+                        },
+                    )),
+                }],
+                condition: None,
+            }))),
+        });
+        plan
+    }
+
     #[tokio::test]
     async fn identity_calc_runs_as_datafusion_projection_without_copying_values() {
         let schema = Arc::new(Schema::new(vec![Field::new("id", DataType::Int32, false)]));
@@ -83,6 +107,28 @@ mod tests {
         let batch = RecordBatch::try_new(schema.clone(), vec![values]).unwrap();
         let source = MemorySourceConfig::try_new_exec(&[vec![batch]], schema, None).unwrap();
         let plan = create_plan(&identity_plan().encode_to_vec(), source).unwrap();
+
+        let output = collect(plan, SessionContext::new().task_ctx())
+            .await
+            .unwrap();
+        let output_values = output[0]
+            .column(0)
+            .as_any()
+            .downcast_ref::<Int32Array>()
+            .unwrap();
+
+        assert_eq!(output_values.values(), &[1, 2, 3]);
+        assert_eq!(output_values.values().as_ptr(), input_pointer);
+    }
+
+    #[tokio::test]
+    async fn adjacent_identity_calcs_share_the_input_arrow_buffer() {
+        let schema = Arc::new(Schema::new(vec![Field::new("id", DataType::Int32, false)]));
+        let values = Arc::new(Int32Array::from(vec![1, 2, 3]));
+        let input_pointer = values.values().as_ptr();
+        let batch = RecordBatch::try_new(schema.clone(), vec![values]).unwrap();
+        let source = MemorySourceConfig::try_new_exec(&[vec![batch]], schema, None).unwrap();
+        let plan = create_plan(&chained_identity_plan().encode_to_vec(), source).unwrap();
 
         let output = collect(plan, SessionContext::new().task_ctx())
             .await
