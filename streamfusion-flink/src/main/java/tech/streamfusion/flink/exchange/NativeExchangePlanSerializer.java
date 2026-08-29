6320a515e5d65566c258c1b4e1d667d6b6a0b8d5
jigglyj2 <jordanistryhardswe@gmail.com>
2026-08-29 09:12:56 -0500

    feat: serialize native exchange plans

---
+++ streamfusion-flink/src/main/java/tech/streamfusion/flink/exchange/NativeExchangePlanSerializer.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2026 StreamFusion Authors
+ * Licensed under the Apache License, Version 2.0
+ */
+package tech.streamfusion.flink.exchange;
+
+import org.apache.flink.table.types.logical.RowType;
+import tech.streamfusion.flink.proto.FlinkLogicalTypeProto;
+import tech.streamfusion.proto.plan.v1.ExchangeDistribution;
+import tech.streamfusion.proto.plan.v1.ExchangeMetadataColumns;
+import tech.streamfusion.proto.plan.v1.ExchangeTransport;
+import tech.streamfusion.proto.plan.v1.Field;
+import tech.streamfusion.proto.plan.v1.NativeExchangePlan;
+import tech.streamfusion.proto.plan.v1.Schema;
+
+/** Serializes the complete Java-side native exchange control contract. */
+public final class NativeExchangePlanSerializer {
+    private NativeExchangePlanSerializer() {}
+
+    public static byte[] hash(RowType rowType, int[] keys, int maxParallelism) {
+        NativeExchangePlan.Builder plan = base(rowType)
+                .setDistribution(ExchangeDistribution.EXCHANGE_DISTRIBUTION_HASH)
+                .setMaxParallelism(maxParallelism);
+        for (int key : keys) {
+            plan.addKeyIndices(key);
+        }
+        return plan.build().toByteArray();
+    }
+
+    public static byte[] singleton(RowType rowType) {
+        return base(rowType)
+                .setDistribution(ExchangeDistribution.EXCHANGE_DISTRIBUTION_SINGLETON)
+                .setMaxParallelism(1)
+                .build()
+                .toByteArray();
+    }
+
+    private static NativeExchangePlan.Builder base(RowType rowType) {
+        RowType exchangeType = ArrowExchangeBatch.exchangeRowType(rowType);
+        Schema.Builder schema = Schema.newBuilder();
+        for (RowType.RowField field : exchangeType.getFields()) {
+            schema.addFields(Field.newBuilder()
+                    .setName(field.getName())
+                    .setType(FlinkLogicalTypeProto.serialize(field.getType())));
+        }
+        int rowKindIndex = rowType.getFieldCount();
+        return NativeExchangePlan.newBuilder()
+                .setProtocolVersion(1)
+                .setSchema(schema)
+                .setTransport(ExchangeTransport.EXCHANGE_TRANSPORT_ARROW_IPC_STREAM)
+                .setMetadataColumns(ExchangeMetadataColumns.newBuilder()
+                        .setRowKindIndex(rowKindIndex)
+                        .setStreamRecordTimestampIndex(rowKindIndex + 1));
+    }
+}
---
+++ streamfusion-flink/src/test/java/tech/streamfusion/flink/exchange/NativeExchangePlanSerializerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2026 StreamFusion Authors
+ * Licensed under the Apache License, Version 2.0
+ */
+package tech.streamfusion.flink.exchange;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.flink.table.types.logical.IntType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.VarCharType;
+import org.junit.jupiter.api.Test;
+import tech.streamfusion.proto.plan.v1.ExchangeDistribution;
+import tech.streamfusion.proto.plan.v1.NativeExchangePlan;
+
+class NativeExchangePlanSerializerTest {
+    @Test
+    void recordsSchemaKeysAndStableFlinkEnvelopeIndices() throws Exception {
+        RowType rowType = RowType.of(
+                new org.apache.flink.table.types.logical.LogicalType[] {new IntType(), new VarCharType()},
+                new String[] {"id", "name"});
+
+        NativeExchangePlan plan =
+                NativeExchangePlan.parseFrom(NativeExchangePlanSerializer.hash(rowType, new int[] {0}, 128));
+
+        assertThat(plan.getDistribution()).isEqualTo(ExchangeDistribution.EXCHANGE_DISTRIBUTION_HASH);
+        assertThat(plan.getKeyIndicesList()).containsExactly(0);
+        assertThat(plan.getMaxParallelism()).isEqualTo(128);
+        assertThat(plan.getSchema().getFieldsList())
+                .extracting(tech.streamfusion.proto.plan.v1.Field::getName)
+                .containsExactly("id", "name", ArrowExchangeBatch.ROW_KIND_COLUMN, ArrowExchangeBatch.TIMESTAMP_COLUMN);
+        assertThat(plan.getMetadataColumns().getRowKindIndex()).isEqualTo(2);
+        assertThat(plan.getMetadataColumns().getStreamRecordTimestampIndex()).isEqualTo(3);
+    }
+}
